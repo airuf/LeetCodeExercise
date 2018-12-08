0a34722f0c6eea0ea81e15e82054231e1a2cdc17
LMAN-PC\LMAN <13526584585@163.com>
2018-12-08 17:18:03 +0800

    Update

--- LeetCode/LeetCode/main.cpp
+++ LeetCode/LeetCode/main.cpp
@@ -1,38 +1,32 @@
 #include "includes.h"
-#include "E-021. MergeTwoSortedLists.h"
+#include "E-203. RemoveLinkedListElements.h"
 
-ListNode *LinkedListCreatT(int i)
+
+void InitLinkedList(ListNode *lst, const vector<int> *a)
 {
-	ListNode *L;
-	L = (ListNode *)malloc(sizeof(ListNode));   //申请头结点空间
-	L->next = NULL;                  //初始化一个空链表
-	ListNode *r;
-	r = L;                          //r始终指向终端结点，开始时指向头结点 
-	int x;                         //x为链表数据域中的数据
-	L->val = i;
-	while (i++<5) {
-		ListNode *p;
-		p = (ListNode *)malloc(sizeof(ListNode));   //申请新的结点 
-		p->val = i;                     //结点数据域赋值 
-		r->next = p;                 //将结点插入到表头L-->|1|-->|2|-->NULL 
-		r = p;
-		
+	ListNode *cp1 = lst;
+	vector<int> _data = *a;
+	ListNode *p;
+	cp1->val = _data[0];
+	for (int _dataLen = _data.size(), i = 1; i < _dataLen; i++)
+	{
+		p = (ListNode*)malloc(sizeof(ListNode));
+		cp1->next = p;
+		cp1 = cp1->next;
+		p->val = _data[i];
+		p->next = NULL;
 	}
-	r->next = NULL;
-
-	return L;
 }
-
 
 int main()
 {
-	ListNode *a = LinkedListCreatT(2);
-	ListNode *b = LinkedListCreatT(3);
-	
-
-	Solution X;
-	
-	ListNode *c = X.mergeTwoLists(a, b);
+	struct ListNode l1(0);
+	//vector<int> a{ 1,2,6,3,4,5,6 };
+	vector<int> a{ 1,1};
+	InitLinkedList(&l1,&a);
+	struct ListNode l2(NULL);
+	Solution S;
+	S.removeElements(&l2, 2);
 
 	system("pause");
 	return 0;
