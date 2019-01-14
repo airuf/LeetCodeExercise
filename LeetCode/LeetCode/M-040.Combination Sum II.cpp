0d4b64bc7ab062d7278733fe48a4afa36f27f2d6
LMAN-PC\LMAN <13526584585@163.com>
2019-01-14 10:04:16 +0800

    temporary save

---
+++ LeetCode/LeetCode/M-040.Combination Sum II.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+	vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
+	{
+		vector<vector<int>> res;
+		if (candidates.size() == 0 || target < 0)
+			return res;
+		
+	}
+
+	int recursionNum(vector<int> copy, int target)
+	{
+		for (int i = 0; i < copy.size(); i++)
+		{
+			if (copy[i] == target)
+				break;
+		}
+	}
+}
+
